893a11ebf4a31078856f2fd207b698d682af317c
LucaStellato <lucastellato435@gmail.com>
2025-10-06 14:41:16 +0200

    add random generation

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -3,3 +3,6 @@ chiedere all'utente se vuole pari o dispari e un numero intero compreso tra 1 e 
 Generare un numero casuale compreso tra 1 e 9, che sarà la giocata el computer. 
 Stabilire e comunicare chi ha vinto secondo le regole di pari o dispadri.
 */
+
+const even_odd = prompt("SFIDAMI!  Scegli... Pari o dispari");
+const cpu = Math.floor(Math.random() * 10) + 1; 
