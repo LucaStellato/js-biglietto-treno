34cf65ce80bc271654edad758c89613c161ddff2
LucaStellato <lucastellato435@gmail.com>
2025-10-06 14:36:01 +0200

    past the track

---
+++ bonus/bonus.js
@@ -0,0 +1,5 @@
+/*pari o dispari: 
+chiedere all'utente se vuole pari o dispari e un numero intero compreso tra 1 e 9. 
+Generare un numero casuale compreso tra 1 e 9, che sarà la giocata el computer. 
+Stabilire e comunicare chi ha vinto secondo le regole di pari o dispadri.
+*/
