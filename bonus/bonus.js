bc5de758694fb38283e73c8330a38d58bed540ce
LucaStellato <lucastellato435@gmail.com>
2025-10-06 15:02:31 +0200

    comment the bonus

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -3,15 +3,23 @@ chiedere all'utente se vuole pari o dispari e un numero intero compreso tra 1 e 
 Generare un numero casuale compreso tra 1 e 9, che sarà la giocata el computer. 
 Stabilire e comunicare chi ha vinto secondo le regole di pari o dispadri.
 */
-
+//declaration variable
 const even_odd = prompt("SFIDAMI!  Scegli... Pari o dispari");
 const player = prompt("BRAVO!  Ora scegli un numero tra 1 e 9");
 const cpu = Math.floor(Math.random() * 10) + 1; 
 
-const sum = player + cpu;
+//sum the number cpu a player
+const somma = player + cpu;
 
-if(sum%2==0){
-    if(even_odd===pari){
-        alert("AAA MI HAI BATTUTO")
+
+//condition in case sum/2===0 
+if(somma%2===0){
+    // and if player choose even
+    if(even_odd===("pari")){
+        //player win
+        alert("AAA MI HAI BATTUTO");
+    }else{
+        //else player lost , win cpu
+        alert("Hai perso AHAHAHAHA RITENTA");
     }
 }
